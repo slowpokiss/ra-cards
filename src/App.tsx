7defb85758a8550f71295ebc0b1fdb74b3372610
Michel <hltvexe@mail.ru>
2024-02-09 13:11:28 +0300

    updated path to images and image component

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,15 @@ import Button from "./components/Button";
 import Image from "./components/Image";
 
 export default function App() {
-  {
-    /* 
-<div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div> 
-*/
-  }
-
   return (
     <>
       <Card>
         <div className="card">
-          <Image srcImg="https://ibb.co/j6n80pR" />
+          <Image
+            srcImg="https://img.freepik.com/free-photo/bright-petals-gift-love-in-a-bouquet-generated-by-ai_188544-13370.jpg"
+            width={"auto"}
+            height={"auto"}
+          />
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
             <p className="card-text">
--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,11 +1,21 @@
 interface props {
-  srcImg: string
+  srcImg: string;
+  width: number | string;
+  height: number | string;
 }
 
-const Image = ({ srcImg }: props) => {
+const Image = ({ srcImg, width, height }: props) => {
   return (
-    <img src={srcImg} className="card-img" />
-  )
-}
+    <img
+      src={srcImg}
+      alt="image"
+      className="card-img"
+      style={{
+        width: `${typeof width === "string" ? width : `${width}px`}`,
+        height: `${typeof height === "string" ? height : `${height}px`}`,
+      }}
+    />
+  );
+};
 
-export default Image
+export default Image;
